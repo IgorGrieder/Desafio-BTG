1eab339283e5d16d4ff81ea8daed242edfb0b814
Igor Grieder Melo <igorgrieder@gmail.com>
2025-12-30 20:07:58 -0300

    new order creation process

--- ms/internal/application/services/order_processing_service.go
+++ ms/internal/application/services/order_processing_service.go
@@ -2,33 +2,73 @@ package services
 
 import (
 	"context"
+	"fmt"
 
-	database "github.com/IgorGrieder/Desafio-BTG/tree/main/ms/internal/adapters/outbound/database"
+	db "github.com/IgorGrieder/Desafio-BTG/tree/main/ms/internal/adapters/outbound/database"
+	database "github.com/IgorGrieder/Desafio-BTG/tree/main/ms/internal/adapters/outbound/database/sqlc"
 	"github.com/IgorGrieder/Desafio-BTG/tree/main/ms/internal/domain"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 // OrderProcessingService handles business logic for processing orders from RabbitMQ
 type OrderProcessingService struct {
-	queries *database.Store
+	queries *db.Store
 }
 
 // NewOrderProcessingService creates a new OrderProcessingService with dependency injection
-func NewOrderProcessingService(queries *database.Store) *OrderProcessingService {
+func NewOrderProcessingService(queries *db.Store) *OrderProcessingService {
 	return &OrderProcessingService{
 		queries: queries,
 	}
 }
 
 // ProcessOrder processes an order message from RabbitMQ and saves to database
-func (s *OrderProcessingService) ProcessOrder(ctx context.Context, orderID, customerID string, amount float64) error {
-	// TODO: Implement business logic with transaction
-	// Example:
-	// 1. Validate order data
-	// 2. Begin transaction
-	// 3. Create order: s.queries.CreateOrder(ctx, params)
-	// 4. Create order items: for each item, s.queries.CreateOrderItem(ctx, params)
-	// 5. Commit transaction
-	// 6. Log success
+func (s *OrderProcessingService) ProcessOrder(ctx context.Context, order *domain.Order) error {
+	tx, err := s.queries.Pool.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return fmt.Errorf("error opening transaction %v", err)
+	}
+	defer tx.Commit(ctx)
+
+	queries := s.queries.WithTx(tx)
+
+	args := database.CreateOrderParams{
+		Code:         int32(order.OrderCode),
+		CustomerCode: int32(order.CustomerCode),
+	}
+
+	orderCreated, err := queries.CreateOrder(ctx, args)
+	if err != nil {
+		tx.Rollback(ctx)
+		return fmt.Errorf("error creating order %v", err)
+	}
+
+	orderId := int64(orderCreated.Code)
+
+	for _, item := range order.Items {
+		var p pgtype.Numeric
+
+		err := p.Scan(item.Price)
+		if err != nil {
+			tx.Rollback(ctx)
+			return err
+		}
+
+		args := database.CreateOrderItemParams{
+			OrderID:  orderId,
+			Product:  item.Product,
+			Price:    p,
+			Quantity: int32(item.Quantity),
+		}
+
+		_, err = queries.CreateOrderItem(ctx, args)
+		if err != nil {
+			tx.Rollback(ctx)
+			return fmt.Errorf("error creating order item %v : %v", item, err)
+		}
+	}
+
 	return nil
 }
 
